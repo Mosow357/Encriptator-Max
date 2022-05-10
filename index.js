14d84743d599875161039ce9a7b50dc53798e59e
walter <arenalwm@gmail.com>
2022-05-10 18:55:59 -0300

    subida encriptator max

---
+++ index.js
@@ -0,0 +1,55 @@
+
+var input = document.getElementById("input");
+var output = document.getElementById("output");
+const btnCod = document.getElementById("btnCod");
+const btnDeCod = document.getElementById("btnDeCod");
+const clear = document.getElementById("clear");
+
+
+
+document.getElementById("input").focus();
+
+
+
+
+btnCod.addEventListener ("click", function () {
+    
+    
+    let cod1 = input.value;
+    let cod = cod1.toLowerCase()
+    
+    
+  
+    
+    cod = cod.replaceAll("e","enter");
+    cod = cod.replaceAll("i","imes");
+    cod = cod.replaceAll("o","ober");
+    cod = cod.replaceAll("u","ufat");
+    cod = cod.replaceAll("a","ai");
+    
+
+    input.value = cod;
+    
+    document.getElementById("input").focus();
+    
+});
+
+btnDeCod.addEventListener ("click", function () {
+    let cod1 = input.value;
+    let cod = cod1.toLowerCase()
+  
+    cod = cod.replaceAll("ai", "a");
+    cod = cod.replaceAll("enter", "e");
+    cod = cod.replaceAll("imes", "i");
+    cod = cod.replaceAll("ober", "o");
+    cod = cod.replaceAll("ufat", "u");
+
+  
+    input.value = cod;
+    
+    document.getElementById("input").focus();
+});
+clear.addEventListener("click",function(){
+    input.value = "";
+    document.getElementById("input").focus();
+})
